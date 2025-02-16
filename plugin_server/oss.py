0b20ee65381b1169ec7a8515d7f88e9b13912413
Circle1688 <986638435@qq.com>
2025-02-16 11:57:08 +0800

    Update oss.py

--- plugin_server/oss.py
+++ plugin_server/oss.py
@@ -43,8 +43,8 @@ def get_files_oss(folder_prefix):
     files = []
     for obj in oss2.ObjectIteratorV2(bucket, prefix=folder_prefix):
         files.append((obj.key, obj.last_modified))
-    # 按最后修改时间排序（从早到晚）
-    files_sorted_by_time = sorted(files, key=lambda x: x[1])
+    # 按最后修改时间排序（从晚到早）
+    files_sorted_by_time = sorted(files, key=lambda x: x[1], reverse=True)
 
     file_list = []
     for key, _ in files_sorted_by_time:
